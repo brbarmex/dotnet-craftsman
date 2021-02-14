74a1a1d26660ea0bf4b49ed32b1b2798113483ee
Bruno Melo <bruno.b.melo@live.com>
2021-02-14 15:46:05 -0300

    Implementing interator

--- FP.DotNet.Domain/Interators/Customer/CreateNewCustomer.cs
+++ FP.DotNet.Domain/Interators/Customer/CreateNewCustomer.cs
@@ -1,6 +1,47 @@
-namespace FP.DotNet.Domain.Interators.Customer
+using System;
+using FP.DotNet.Domain.Bases;
+using FP.DotNet.Shared.Interfaces;
+using OneOf;
+using FP.DotNet.Shared.Commands;
+using FP.DotNet.Domain.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FP.DotNet.Domain.Interators.CustomerServices
 {
     public class CreateNewCustomer
     {
+        private readonly INotifications _notification;
+
+        public CreateNewCustomer(INotifications notifications)
+        => _notification = notifications;
+
+        public OneOf<IReadOnlyCollection<Notification>, Customer, Exception> Execute(NewCustomer input)
+        {
+            try
+            {
+                var model = new Customer(
+                    name: input.Name,
+                    fullName: input.FullName,
+                    cpf: input.Cpf,
+                    email: input.Email,
+                    birthDate: input.BirthDate,
+                    street: input.Street,
+                    zipcode: input.ZipCode,
+                    city: input.City,
+                    country: input.Country);
+
+                if (!model.IsValid)
+                    _notification.AddNotification(model.Notifications);
+
+                return _notification.HasNotifications()
+                        ? _notification.GetNotifications().ToList()
+                        : model;
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
     }
 }
---
+++ FP.DotNet.Shared/Commands/NewCustomer.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FP.DotNet.Shared.Commands
+{
+    public class NewCustomer
+    {
+        public string Name { get; }
+        public string FullName { get; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
