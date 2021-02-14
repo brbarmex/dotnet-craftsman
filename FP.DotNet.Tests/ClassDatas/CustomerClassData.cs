4d48f55d50703f1524e17f78f92bda7a1567f3b3
Bruno Melo <bruno.b.melo@live.com>
2021-02-14 14:43:55 -0300

    Added more tests

--- FP.DotNet.Tests/ClassDatas/CustomerClassData.cs
+++ FP.DotNet.Tests/ClassDatas/CustomerClassData.cs
@@ -45,6 +45,30 @@ namespace FP.DotNet.Tests.ClassDatas
                             string.Empty,
                             string.Empty),
                 };
+            yield return new object[]{
+             0,
+                new Customer("Bruno",
+                            "Barbosa de Melo",
+                            "172.236.500-52",
+                            "bruno.b.melo@live.com",
+                            new DateTime(1994,07,30),
+                            "My address",
+                            "04173-020",
+                            "São Paulo",
+                            "Brazil"),
+                };
+            yield return new object[]{
+             1,
+                new Customer("Bruno",
+                            "Barbosa de Melo",
+                            "172.236.500-51",
+                            "bruno.b.melo@live.com",
+                            new DateTime(1994,07,30),
+                            "My address",
+                            "04173-020",
+                            "São Paulo",
+                            "Brazil"),
+                };
         }
 
         IEnumerator IEnumerable.GetEnumerator()
