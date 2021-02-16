5c93daeaa78ad63c4070f3d9a8e80e108c2f0ad9
Bruno Melo <bruno.b.melo@live.com>
2021-02-16 16:32:25 -0300

    Refactorin Constants

--- Craftsman.Shared/Constants/Constant.cs
+++ Craftsman.Shared/Constants/Constant.cs
@@ -1,18 +1,26 @@
 namespace Craftsman.Shared.Constants
 {
-    public static class Constant
+    public static class Message
     {
-        public readonly static Message Message = new();
-        public readonly static Key Key = new();
+         public static readonly string ValueNotExistingInTheBrazilianTerritory = "CEP not existing in the Brazilian territory";
+         public static readonly string MusteBeAnAdult = "Must be an adult.";
+
+         public static string ThePropertyIsInvalidPleaseVerify(string key)
+         => $"The {key} is invalid please verify.";
+
+         public static string InvalidNumberOfCharacters(string key, int min, int max)
+         => $"{key} : Invalid number of characters (min: {min} max:{max})";
     }
 
-    public class Message
+    public static class PropertyName
     {
-         public readonly string ValueNotExistingInTheBrazilianTerritory = "CEP not existing in the Brazilian territory";
-    }
-
-    public class Key
-    {
-        public readonly string ZipCode = "zipcode";
+        public static readonly string ZipCode = "zipcode";
+        public static readonly string CPF = "cpf";
+        public static readonly string Email = "e-mail";
+        public static readonly string Name = "name";
+        public static readonly string FullName = "full_name";
+        public static readonly string City = "city";
+        public static readonly string Street = "street";
+        public static readonly string Country = "country";
     }
 }
